0b09ccf1aeb147f8834ab15cb91ae3ea9e532549
Debasish Sahoo <debasish.sahoo1998@gmail.com>
2018-05-25 14:59:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 module.exports = robot => {
   robot.on('issues.opened', async context => {
-  
     const params = context.issue({body: 'Hello World!'})
-
-    // Post a comment on the issue
     return context.github.issues.createComment(params)
   })
 }
